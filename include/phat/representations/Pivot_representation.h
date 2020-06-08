baf9282c5cb2e2cae186b5300bced0895495c3f6
Michael Kerber <mkerber@fgeompc15.tugraz.local>
2020-06-08 12:55:38 +0200

    Missing return

--- include/phat/representations/Pivot_representation.h
+++ include/phat/representations/Pivot_representation.h
@@ -110,7 +110,7 @@ template<typename BaseRepresentation, typename PivotColumn>
 
   void _remove_max( index idx ) { is_pivot_col( idx ) ? get_pivot_col().remove_max() : Base::_remove_max( idx ); }
 
-  index _size( index idx) { is_pivot_col( idx ) ? get_pivot_col().size() : Base::_size( idx ); }
+  index _size( index idx) { return is_pivot_col( idx ) ? get_pivot_col().size() : Base::_size( idx ); }
 
   void _swap(index idx1, index idx2 ) {
     if ( is_pivot_col( idx1 ) || is_pivot_col( idx2 ) ) {
