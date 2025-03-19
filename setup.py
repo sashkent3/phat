999b8180d5d1576ba643ba3917a6fb158c3fbd2d
Alexander Zinov <33320473+sashkent3@users.noreply.github.com>
2025-03-19 18:06:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,8 +44,6 @@ class BuildExt(build_ext):
             ext.include_dirs.append(pybind11.get_include(user=True))
         build_ext.build_extensions(self)
 
-requires = ['setuptools', 'pybind11']
-
 if sys.version_info < (3,4,0):
     requires.append('enum34')
 
